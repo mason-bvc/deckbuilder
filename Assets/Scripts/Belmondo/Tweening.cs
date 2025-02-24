1b0284872b34ac4802b2c592f14b348f8586e1d1
mason-bvc <m.watfa673@mybvc.ca>
2025-02-24 14:37:01 -0700

    More tweening fixes

--- Assets/Scripts/Belmondo/Tweening.cs
+++ Assets/Scripts/Belmondo/Tweening.cs
@@ -12,43 +12,69 @@ namespace Belmondo
         Finished,
     }
 
-    public class Tween<T>
+    public class Tween<T> where T : new()
     {
-        private float _t;
-        public T From { get; private set; }
-        public T To { get; private set; }
+        public delegate T LerpDelegate(T from, T to, float t);
+
+        public float CurrentT { get; private set; }
+        public LerpDelegate? Lerp { get; private set; }
+        public Action<T>? Setter { get; private set; }
+        public T From { get; private set; } = new();
+        public T To { get; private set; } = new();
         public TimeSpan Duration { get; private set; }
         public TweenStatus Status { get; private set; }
-
-        public Tween(T from, T to, TimeSpan duration)
-        {
-            From = from;
-            To = to;
-            Duration = duration;
-        }
+        public Task? Task { get; private set; }
 
         public void Update(TimeSpan deltaTime)
         {
-            if (Status != TweenStatus.Running)
+            bool shouldUpdate = true;
+
+            shouldUpdate = shouldUpdate && Status == TweenStatus.Running;
+            shouldUpdate = shouldUpdate && Setter is not null;
+            shouldUpdate = shouldUpdate && Lerp is not null;
+
+            // blagh
+            if (!shouldUpdate)
             {
                 return;
             }
 
-            _t += (float)(deltaTime / Duration);
-            _t = Math.Clamp(_t, 0.0F, 1.0F);
+            CurrentT += (float)(deltaTime / Duration);
+            CurrentT = Math.Clamp(CurrentT, 0.0F, 1.0F);
+            Setter!.Invoke(Lerp!.Invoke(From, To, CurrentT));
 
-            if (_t >= 1.0F)
+            if (CurrentT == 1.0F)
             {
                 Status = TweenStatus.Finished;
             }
         }
 
-        public Task Run()
+        public Tween<T> SetLerpDelegate(LerpDelegate @delegate)
         {
-            return new Task(() =>
+            Lerp = @delegate;
+            return this;
+        }
+
+        public Tween<T> SetSetterDelegate(Action<T> @delegate)
+        {
+            Setter = @delegate;
+            return this;
+        }
+
+        public Task Run(T from, T to, TimeSpan duration)
+        {
+            CurrentT = 0;
+            From = from;
+            To = to;
+            Duration = duration;
+            Status = TweenStatus.Running;
+
+            Task = Task.Run(() =>
             {
                 while (Status != TweenStatus.Finished) ;
             });
+
+            return Task;
         }
     }
 }
